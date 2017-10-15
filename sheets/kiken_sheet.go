e753fcfbd55900c95d94df0844691d7551bbd78a
tdaira <asada09403@gmail.com>
2017-10-15 18:04:43 +0900

    Ignore registerd date.

--- sheets/kiken_sheet.go
+++ sheets/kiken_sheet.go
@@ -3,6 +3,7 @@ package sheets
 import (
 	"errors"
 	"strconv"
+	"strings"
 	"time"
 
 	sheets "google.golang.org/api/sheets/v4"
@@ -86,6 +87,13 @@ func (s *KikenSheet) AddDate(userName string, dates []time.Time) error {
 			writeDates := appendDate
 			if len(userDates) > i {
 				if userDates[i].(string) != "" {
+					contains, err := s.containsDates(userDates[i].(string), dates)
+					if err != nil {
+						return err
+					}
+					if contains {
+						return errors.New("Date contains previous application date")
+					}
 					writeDates = userDates[i].(string) + "," + appendDate
 				}
 			}
@@ -99,4 +107,48 @@ func (s *KikenSheet) AddDate(userName string, dates []time.Time) error {
 		}
 	}
 	return errors.New("User not found")
+}
+
+func (s *KikenSheet) containsDates(prevDates string, dates []time.Time) (bool, error) {
+	rangeDates := [][]time.Time{{}}
+	for _, date := range strings.Split(prevDates, ",") {
+		rangeDate := []time.Time{}
+		if strings.Contains(date, "-") {
+			spritDate := strings.Split(date, "-")
+			day1, err := time.Parse("2006/1/2", spritDate[0])
+			if err != nil {
+				return false, err
+			}
+			day2, err := time.Parse("2006/1/2", spritDate[1])
+			if err != nil {
+				return false, err
+			}
+			rangeDate = append(rangeDate, day1)
+			rangeDate = append(rangeDate, day2)
+		} else {
+			day1, err := time.Parse("2006/1/2", date)
+			if err != nil {
+				return false, err
+			}
+			rangeDate = append(rangeDate, day1)
+		}
+		rangeDates = append(rangeDates, rangeDate)
+	}
+
+	for _, cDate := range dates {
+		noNoizeDate := time.Date(cDate.Year(), cDate.Month(), cDate.Day(), 0, 0, 0, 0, time.UTC)
+		for _, pDates := range rangeDates {
+			if len(pDates) == 1 {
+				if noNoizeDate.Equal(pDates[0]) {
+					return true, nil
+				}
+			} else if len(pDates) == 2 {
+				if (noNoizeDate.After(pDates[0]) && noNoizeDate.Before(pDates[2])) ||
+					noNoizeDate.Equal(pDates[0]) || noNoizeDate.Equal(pDates[1]) {
+					return true, nil
+				}
+			}
+		}
+	}
+	return false, nil
 }
