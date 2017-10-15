a35dac7010c4b419495463cf6031e9ca979487da
odaira09403 <koiru4423isako@yahoo.co.jp>
2017-10-15 16:28:49 +0900

    Add kiken date to spread sheet.

--- sheets/kiken_sheet.go
+++ sheets/kiken_sheet.go
@@ -2,6 +2,8 @@ package sheets
 
 import (
 	"errors"
+	"strconv"
+	"time"
 
 	sheets "google.golang.org/api/sheets/v4"
 )
@@ -55,4 +57,46 @@ func (s *KikenSheet) AddNewUser(userName string) error {
 		return err
 	}
 	return nil
+}
+
+// AddDate adds new date of kiken.
+func (s *KikenSheet) AddDate(userName string, dates []time.Time) error {
+	appendDate := ""
+	if len(dates) == 1 {
+		appendDate = dates[0].Format("2006/01/02")
+	} else if len(dates) == 2 {
+		appendDate = dates[0].Format("2006/01/02") + "-" + dates[1].Format("2006/01/02")
+	} else {
+		return errors.New("Invalid length of dates")
+	}
+	ret, err := s.Sheets.Values.Get(SpreadSheetID, "kiken!A2:B").MajorDimension("COLUMNS").Do()
+	if err != nil {
+		return err
+	}
+	if len(ret.Values) < 1 {
+		return errors.New("User not found")
+	}
+	users := ret.Values[0]
+	userDates := []interface{}{}
+	if len(ret.Values) >= 2 {
+		userDates = ret.Values[1]
+	}
+	for i, user := range users {
+		if user.(string) == userName {
+			writeDates := appendDate
+			if len(userDates) > i {
+				if userDates[i].(string) != "" {
+					writeDates = userDates[i].(string) + "," + appendDate
+				}
+			}
+			_, err = s.Sheets.Values.Update(SpreadSheetID, "kiken!B"+strconv.Itoa(i+2), &sheets.ValueRange{
+				Values: [][]interface{}{[]interface{}{writeDates}},
+			}).ValueInputOption("USER_ENTERED").Do()
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return errors.New("User not found")
 }
